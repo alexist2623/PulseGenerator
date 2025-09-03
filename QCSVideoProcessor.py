bd83ef50bf7cf9a25989db4a4add66b485223108
Jeonghyun Park <49219392+alexist2623@users.noreply.github.com>
2025-09-02 20:43:29 -0700

    physical tuning with qtt

---
+++ QCSVideoProcessor.py
@@ -0,0 +1,336 @@
+import numpy as np
+import warnings
+import logging
+import copy
+import qcodes
+import ast
+
+from keysight import qcs
+import qtt
+from qtt.measurements.videomode_processor import VideoModeProcessor
+from qtt.measurements.scans import makeDataset_sweep, makeDataset_sweep_2D
+from qtt.measurements.acquisition.interfaces import AcquisitionScopeInterface
+
+##############################################
+# Parameter Definition
+##############################################
+V_m5301AWG  = 2.5
+
+ns          = 1e-9
+us          = 1e-6
+GHz         = 1e9
+MHz         = 1e6
+
+mV          = 1/(V_m5301AWG * 1000)
+V           = 1/(V_m5301AWG)
+
+class QCSVideomodeProcessor(VideoModeProcessor):
+    """
+    Videomode processor for QCS
+    """
+
+    def __init__(
+            self,
+            station,
+            model_name,
+            verbose = 1,
+        ):
+        self.station = station
+        self.verbose = verbose
+        self.sweepparams = []
+        self.sweepranges = []
+        self.sweep_resolution = []
+        self.minstrumenthandle = "qcs"
+        self.channels = []
+        self.gate_mapping = station[model_name].gate_mapping
+        self.rf_mapping = station[model_name].rf_mapping
+        self.dig_mapping = station[model_name].dig_mapping
+        self.meas_setup = station[model_name].meas_setup
+
+        self.scan_parameters = {}
+        self.period_1d = qcodes.ManualParameter(
+            'period_1d',
+            initial_value = 1e-3
+        )
+        self.qcs_program = qcs.Program()
+        self.qcs_mapper = qcs.ChannelMapper()
+        self.qcs_gates = {}
+        self.qcs_rf = {}
+        self.qcs_dig = {}
+        self.qcs_gate_v_scalar = {}
+        self.qcs_program_generated = False
+        self._generate_qcs_program()
+        self.qcs_program_generated = True
+        self.qcs_backend = qcs.HclBackend(
+            channel_mapper = self.qcs_mapper,
+            hw_demod = True,
+            init_time = 70e-9,
+            suppress_rounding_warnings = True,
+            keep_progress_bar = False
+        )
+    
+    def _generate_qcs_program(self):
+        for gate_name, dac_addr in self.gate_mapping.items():
+            self.qcs_gates[gate_name] = qcs.Channels(
+                labels = 0,
+                name = gate_name,
+            )
+            print(self.qcs_gates[gate_name])
+            print(dac_addr)
+            self.qcs_mapper.add_channel_mapping(
+                channels = self.qcs_gates[gate_name],
+                addresses= [dac_addr],
+                instrument_types = qcs.InstrumentEnum.M5301AWG
+            )
+        for rf_name, rf_addr in self.rf_mapping.items():
+            self.qcs_rf[rf_name] = qcs.Channels(
+                labels = 0,
+                name = rf_name,
+                absolute_phase = True
+            )
+            self.qcs_mapper.add_channel_mapping(
+                self.qcs_rf[rf_name],
+                [rf_addr],
+                qcs.InstrumentEnum.M5300AWG
+            )
+        for dig_name, dig_addr in self.dig_mapping.items():
+            self.qcs_dig[dig_name] = qcs.Channels(
+                labels = 0,
+                name = dig_name,
+                absolute_phase = True
+            )
+            self.qcs_mapper.add_channel_mapping(
+                self.qcs_dig[dig_name],
+                [dig_addr],
+                qcs.InstrumentEnum.M5200Digitizer
+            )
+        for gate_name, _ in self.gate_mapping.items():
+            v = self.station.gates.parameters[gate_name].get()
+            self.qcs_gate_v_scalar[gate_name] = qcs.Scalar(
+                name = gate_name + "_amp",
+                dtype=float,
+                value = self.station.gates.parameters[gate_name].get() * mV
+            )
+
+        meas = self.meas_setup
+        if hasattr(meas, "downconverter"):
+            self.qcs_mapper.add_downconverters(
+                self.dig_mapping[meas["dig"]],
+                ast.literal_eval(meas["downconverter"])
+            )
+            self.qcs_mapper.set_lo_frequencies(
+                [
+                    ast.literal_eval(meas["downconverter"]),
+                    self.rf_mapping[meas["rf"]]
+                ],
+                meas["frequency"] - 50e6
+            )
+        else:
+            self.qcs_mapper.set_lo_frequencies(
+                [
+                    self.rf_mapping[meas["rf"]]
+                ],
+                0
+            )
+        for gate_name, _ in self.gate_mapping.items():
+            # DC Waveform
+            waveform = qcs.DCWaveform(
+                name = gate_name + "_waveform",
+                duration = meas["gate_time"] + meas["acquisition_time"],
+                envelope = qcs.ConstantEnvelope(),
+                amplitude= self.qcs_gate_v_scalar[gate_name]
+            )
+            self.qcs_program.add_waveform(
+                waveform,
+                self.qcs_gates[gate_name]
+            )
+        # RF Waveform
+        waveform = qcs.RFWaveform(
+            name = meas["rf"] + "_waveform",
+            duration = meas["acquisition_time"],
+            envelope = qcs.GaussianEnvelope(),
+            amplitude= 1.0,
+            rf_frequency= meas["frequency"]
+        )
+        self.qcs_program.add_waveform(
+            qcs.Delay(meas["gate_time"]),
+            self.qcs_rf[meas["rf"]]
+        )
+        self.qcs_program.add_waveform(
+            waveform,
+            self.qcs_rf[meas["rf"]]
+        )
+        self.qcs_program.add_acquisition(
+            integration_filter = waveform,
+            channels = self.qcs_dig[meas["dig"]],
+            pre_delay = meas["gate_time"]
+        )
+        self.channels.append(meas["dig"])
+        self.unique_channels = list(np.unique(self.channels))
+
+        for gate_name, sweep_value in meas["sweepparams"].items():
+            self.sweepparams.append(gate_name)
+            self.sweepranges.append(sweep_value["range"])
+            sweep = qcs.Array(
+                name = gate_name + "_array",
+                value = np.array(
+                    [
+                        (
+                            self.station.gates.parameters[gate_name].get()
+                            + x * sweep_value["range"]/(sweep_value["resolution"]-1)
+                        ) * mV for x in range(sweep_value["resolution"])
+                    ]
+                )
+            )
+            self.qcs_program.sweep(
+                sweep,
+                self.qcs_gate_v_scalar[gate_name]
+            )
+            self.sweep_resolution.append(sweep_value["resolution"])
+        self.qcs_program.n_shots(meas["n_shots"])
+        self.n_shots = meas["n_shots"]
+
+    def plot_title(self, index):
+        plot_title = str(self.minstrumenthandle) + ' ' + str(self.channels[index])
+        return plot_title
+
+    def create_dataset(self, processed_data, metadata):
+        alldata = [None] * len(processed_data)
+        if processed_data.ndim == 2:
+            for jj, data_block in enumerate(processed_data):
+                dataset, _ = makeDataset_sweep(
+                    data_block,
+                    self.sweepparams,
+                    self.sweepranges[0],
+                    gates=self.station.gates,
+                    loc_record={'label': 'videomode_1d'}
+                )
+                dataset.metadata = copy.copy(metadata)
+                alldata[jj] = dataset
+        elif processed_data.ndim == 3:
+            for jj, data_block in enumerate(processed_data):
+                dataset, _ = makeDataset_sweep_2D(
+                    data_block,
+                    self.station.gates,
+                    self.sweepparams,
+                    self.sweepranges,
+                    loc_record={'label': 'videomode_2d'}
+                )
+                dataset.metadata = copy.copy(metadata)
+                alldata[jj] = dataset
+        else:
+            raise Exception('makeDataset: data.ndim %d' % processed_data.ndim)
+
+        return alldata
+
+    def update_position(self, position, verbose=1):
+        if verbose:
+            print('# %s: update position: %s' % (self.__class__.__name__, position,))
+
+        station = self.station
+        if self.scan_dimension() == 1:
+            delta = position[0]
+            if isinstance(self.scan_parameters['sweepparams'], str):
+                param = getattr(station.gates, self.scan_parameters['sweepparams'])
+                param.set(delta)
+                if verbose > 2:
+                    print('  set %s to %s' % (param, delta))
+                return
+        try:
+            for ii, parameter in enumerate(self.scan_parameters['sweepparams']):
+                delta = position[ii]
+                if verbose:
+                    print('param %s: delta %.3f' % (parameter, delta))
+
+                if isinstance(parameter, str):
+                    if parameter == 'gates_horz' or parameter == 'gates_vert':
+                        d = self.scan_parameters['sweepparams'][parameter]
+                        for gate, factor in d.items():
+                            if verbose >= 2:
+                                print('  %s: increment %s with %s' % (parameter, gate, factor * delta))
+                            param = getattr(station.gates, gate)
+                            param.increment(factor * delta)
+                    else:
+                        if verbose > 2:
+                            print('  set %s to %s' % (parameter, delta))
+                        param = getattr(station.gates, parameter)
+                        param.set(delta)
+                else:
+                    raise Exception('_update_position with parameter type %s not supported' % (type(parameter),))
+        except Exception as ex:
+            logging.exception(ex)
+
+    def ppt_notes(self):
+        return str(self.scan_parameters)
+
+    def initialize(self, videomode):
+        if not self.qcs_program_generated:
+            self._generate_qcs_program()
+
+    def stop(self):
+        pass
+
+    def measure(self, videomode):
+        device_parameters = {}
+        self._device_parameters = device_parameters
+        for gate_name, _ in self.gate_mapping.items():
+            if not gate_name in self.sweepparams:
+                amp_scalar = getattr(self.qcs_program.variables, gate_name + "_amp")
+                amp_scalar.value = self.station.gates[gate_name].get() * mV
+        
+        meas = self.meas_setup
+        for gate_name, sweep_value in meas["sweepparams"].items():
+            arr = getattr(self.qcs_program.variables, gate_name + "_array")
+            arr.value = np.array(
+                    [
+                        (
+                            self.station.gates.parameters[gate_name].get()
+                            + x * sweep_value["range"]/(sweep_value["resolution"]-1)
+                        ) * mV for x in range(sweep_value["resolution"])
+                    ]
+                )
+        data = qcs.Executor(self.qcs_backend).execute(self.qcs_program)
+        data = np.abs(data.get_iq().to_numpy()).reshape(-1, self.n_shots).mean(axis=1)
+        data = [
+            data.reshape(
+                self.sweep_resolution[1],
+                self.sweep_resolution[0],
+            )
+        ]
+        if np.all(data == 0):
+            raise Exception('data returned contained only zeros, aborting')
+        return data
+
+    def process(self, measurement_data, videomode):
+        measurement_data_selected = []
+        for _, channel in enumerate(self.channels):
+            unique_channel_idx = self.unique_channels.index(channel)
+            data_processed = np.array(measurement_data[unique_channel_idx])
+            measurement_data_selected.append(data_processed)
+
+        dd = self.default_processing(measurement_data_selected, videomode)
+        return dd
+
+    def parse_instrument(self, measurement_instrument_handle, sample_rate):
+        # TODO Calculate FPS
+        return self.sampling_frequency
+
+    def scan_dimension(self):
+        if isinstance(self.sweepranges, (float, int)):
+            return 1
+        elif isinstance(self.sweepranges, list):
+            if len(self.sweepranges) == 2:
+                return 2
+            elif len(self.sweepranges) == 1:
+                return 1
+            else:
+                raise Exception('scan dimension not supported')
+        else:
+            return -1
+
+    def extend_videomode_name(self, name):
+        """ String to append to VideoMode name """
+        if name is not None:
+            if isinstance(self.sweepparams, (str, list)):
+                name += ': %s' % str(self.sweepparams)
+        return name
---
+++ qstl_dots.py
@@ -0,0 +1,196 @@
+""" Dot array for QSTL
+
+There are virtual instruments for
+
+- DACs: several virtual IVVIs
+- A virtual gates object
+ """
+
+
+# %% Load packages
+
+import logging
+import threading
+import json
+import ast
+from functools import partial
+from typing import Any, Dict, Mapping, Optional, Tuple
+
+import numpy as np
+import qcodes
+from qcodes import Instrument
+from qcodes.utils.validators import Numbers
+
+from qtt.instrument_drivers.virtual_gates import VirtualGates
+
+logger = logging.getLogger(__name__)
+
+# %%
+class QSTLGate(Instrument):
+    def __init__(
+            self,
+            name,
+            gate_mapping,
+            **kwargs
+        ):
+        super().__init__(name, **kwargs)
+        self.gates = [gate_name for gate_name, _ in gate_mapping.items()]
+        self._state = {g: 0.0 for g in self.gates}
+        for gate in self.gates:
+            self.add_parameter(
+                gate,
+                label="%s" % gate,  # (\u03bcV)',
+                unit="mV",
+                get_cmd=partial(self._get, gate=gate),
+                set_cmd=partial(self._set, gate=gate),
+            )
+    def _get(self, gate: str) -> float:
+        return self._state[gate]
+
+    def _set(self, value: float, gate: str) -> None:
+        self._state[gate] = value
+    
+    def set_boundaries(self, boundaries : dict) -> None:
+        for gate, bd in boundaries.items():
+            self.parameters[gate].vals = Numbers(bd[0], bd[1])
+    
+    def ask_raw(self, cmd: str) -> str:
+        if cmd == "*IDN?":
+            return self.name
+
+    def allvalues(self, get_latest=False):
+        """Return all virtual gate voltage values in a dict."""
+        if get_latest:
+            vals = [(gate, self.parameters[gate].get_latest()) for gate in self.gates]
+        else:
+            vals = [(gate, self.get(gate)) for gate in self.gates]
+        return dict(vals)
+    
+
+class QSTLDotModel(Instrument):
+    """ Simulation model for linear dot array
+
+    The model is intended for testing the code and learning. 
+    It does _not_ simulate any meaningful physics.
+
+    """
+
+    def __init__(
+            self,
+            name: str,
+            verbose: int = 0,
+            dot_configuration: str = None,
+            virtual_gate: bool = False,
+            **kwargs
+        ):
+        """
+        Spin dot model
+
+            Args:
+                name  name for the instrument
+                verbose : verbosity level
+                dot_configuration : dot configuration
+                {
+                    "gate_mapping" : {
+                        "G0"  : "(1, 4, 1)", # Gate Name : (chassis, slot, channel)
+                        "G1"  : "(1, 4, 2)",
+                        "SD0" : "(1, 4, 3)"
+                    },
+                    "dig_mapping" : {
+                        "Dig0" : "(1, 13, 1)", # Digitizer Name : (chassis, slot, channel)
+                        "Dig1" : "(1, 13, 2)"
+                    },
+                    "gate_boundary" : {
+                        "G0" : "(-300, 450)" # Gate name : (min, max) unit is mV
+                    },
+                    "rf_mapping" : {
+                        "RF0" : "(1,3,1)"
+                    },
+                    "meas_setup" : {
+                        "rf" : "RF0",
+                        "dig" : "Dig0",
+                        "frequency" : 200e6,
+                        "gate_time" : 5e-6,
+                        "acquisition_time" : 5e-6,
+                        "sweepparams" : {
+                            "G0" : {
+                                "min" : -10,
+                                "max" : 15,
+                                "num" : 30
+                            },
+                            "G1" : {
+                                "min" : -20,
+                                "max" : 5,
+                                "num" : 30
+                            }
+                        }
+                    }
+                }
+
+        """
+
+        super().__init__(name, **kwargs)
+
+        if dot_configuration is None:
+            raise ValueError("Dot configuration is empty...")
+        else:
+            with open(dot_configuration, "r", encoding="utf-8") as _config:
+                _config_data = json.load(_config)
+
+                self.gate_mapping = _config_data["gate_mapping"]
+                for gate_name, dac_addr in self.gate_mapping.items():
+                    self.gate_mapping[gate_name] = ast.literal_eval(dac_addr)
+
+                self.dig_mapping = _config_data["dig_mapping"]
+                for dig_name, dig_addr in self.dig_mapping.items():
+                    self.dig_mapping[dig_name] = ast.literal_eval(dig_addr)
+                
+                self.rf_mapping = _config_data["rf_mapping"]
+                for rf_name, rf_addr in self.rf_mapping.items():
+                    self.rf_mapping[rf_name] = ast.literal_eval(rf_addr)
+
+                self.gate_boundary = _config_data["gate_boundary"]
+                for gate_name, gate_bound in self.gate_boundary.items():
+                    self.gate_boundary[gate_name] = ast.literal_eval(gate_bound)
+                
+                self.meas_setup = _config_data["meas_setup"]
+
+        # dictionary to hold the data of the model
+        self._data: Dict = {}
+        self.lock = threading.Lock()
+        self.virtual_gate = virtual_gate
+
+        if not virtual_gate:
+            self.gates = QSTLGate(
+                "gates",
+                self.gate_mapping
+            )
+        else:
+            pass
+        self.gates.set_boundaries(self.gate_boundary)
+
+        station = qcodes.Station(
+            self.gates,
+            self,
+            update_snapshot=False
+        )
+        station.metadata['sample'] = 'virtual_dot'
+        station.model = self
+        station.gate_settle = lambda: 0
+        station.jobmanager = None
+        station.calib_master = None
+
+        self._initialized = True
+
+    def _data_get(self, param):
+        return self._data.get(param, 0)
+
+    def _data_set(self, param, value):
+        self._data[param] = value
+        return
+
+# %%
+
+
+if __name__ == '__main__' and 1:
+    pass
