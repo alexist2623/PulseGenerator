95d4ff62b516e39b71ec77440c901c2acdc5a004
Jeonghyun Park <alexist@snu.ac.kr>
2025-10-08 16:10:18 -0700

    feature : tcl is added

--- QES_Pathwave/IP/TraceTrig/TraceTrig.tcl
+++ QES_Pathwave/IP/TraceTrig/TraceTrig.tcl
@@ -1,1 +1,17 @@
-# Vivado Version : 17.3
+# ============================================================
+# Vivado 2017.3  |  Pack "TraceTrig.v" into a reusable IP
+# Usage (example):
+#   vivado -mode batch -source TraceTrig.tcl
+# ============================================================
+set project_name "TraceTrig"
+set project_dir "D:/IP/TraceTrig"
+create_project ${project_name} ${project_dir}/${project_name} -part xc7k325tffg676-2
+
+add_files -norecurse {D:/IP/TraceTrig/TraceTrig.v}
+set boardpath {D:/Xilinx/Vivado/2017.3/data/boards/board_files}
+set_property top TraceTrig [current_fileset]
+set_property top_file { D:/IP/TraceTrig/TraceTrig.v } [current_fileset]
+ipx::package_project -root_dir D:/IP/TraceTrig -vendor xilinx.com -library user -taxonomy /UserIP
+ipx::save_core [ipx::current_core]
+set_property  ip_repo_paths  D:/IP/TraceTrig [current_project]
+update_ip_catalog
